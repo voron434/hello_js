26427f8ca62ca4d0d99138509c5b0c1cda866169
Ilya Osipov <070809010@mail.ru>
2018-03-22 16:28:24 +0300

    Fix main loop and console errors from reddit

--- homework1/index.js
+++ homework1/index.js
@@ -1,4 +1,3 @@
-
 const url = 'https://www.reddit.com/top.json'
 
 
@@ -17,19 +16,28 @@ const splitUpTitles = (jsonResponse) => jsonResponse.data.children.reduce(
 
 
 const renderPosts = (posts) => posts.forEach(
-  (post) => document.getElementById('posts').innerHTML += `
-      <li class="clearfix">
-          <img src="${post.thumbnail}" onerror="this.src='default-thumb.png'" alt="default thumb" class="thumb">
-          <a href="${post.permalink}">
-            <h3>${post.title}</h3>
-          </a>
-      </li>`
+  (post) => {
+    if (post.thumbnail == "default" ||
+        post.thumbnail == "nsfw" ||
+        post.thumbnail == "self") {
+      post.thumbnail = "default-thumb.png"
+    }
+    document.getElementById('posts').innerHTML += `
+        <li class="clearfix">
+            <img src="${post.thumbnail}" onerror="this.src='default-thumb.png'" alt="default thumb" class="thumb">
+            <a href="${post.permalink}">
+              <h3>${post.title}</h3>
+            </a>
+        </li>`
+  }
 )
+
 
 const main = () => fetch(url)
     .then((response) => response.json())
     .then(splitUpTitles)
     .then(renderPosts)
 
-setTimeout( main, 10000 )
+
 main()
+setInterval( main, 10000 )
